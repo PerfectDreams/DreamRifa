3a865ad1d136f7a4fa1847d0d169c575652e0748
MrGaabriel <gabrielbellot7@gmail.com>
2019-03-17 21:56:03 -0300

    okok

--- src/main/kotlin/net/perfectdreams/dreamrifa/DreamRifa.kt
+++ src/main/kotlin/net/perfectdreams/dreamrifa/DreamRifa.kt
@@ -62,7 +62,7 @@ class DreamRifa : KotlinPlugin() {
 			data.lastWinner = winner.uniqueId
 
 			val displayName = Bukkit.getPlayerExact(winner.name)?.displayName ?: winner.name
-			val money = data.players.sumBy { it.tickets }
+			val money = data.players.sumBy { it.tickets * 250 }
 
 			data.lastWinnerPrize = money.toDouble()
 			broadcast("$PREFIX §b${displayName}§e ganhou a rifa! Parabéns! Prêmio: §2${money} Sonhos§e")
